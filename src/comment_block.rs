f3214bfa92c2553033ab1ce78c0e432c438c1df7
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-02 22:28:40 +0200

    feat: implement CommentBlock

---
+++ src/comment_block.rs
@@ -0,0 +1,18 @@
+use crate::binary_data::BinaryData;
+
+struct CommentBlock(String);
+
+impl BinaryData<CommentBlock> for CommentBlock {
+    fn from_bytes(bytes: &[u8]) -> Result<CommentBlock, ()> {
+        let are_all_chars_representable = bytes.iter().all(|byte| *byte >= 33 && *byte <= 126);
+        
+        match are_all_chars_representable {
+            true => Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())), //safe unwrap because all bytes are valid ASCII
+            false => Err(()) //TODO: explicit error
+        }
+    }
+
+    fn to_bytes(&self) -> Result<Vec<u8>, ()> {
+        Ok(self.0.clone().into_bytes())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod block;
 mod binary_data;
+mod header_block;
+mod comment_block;
 
 fn main() {
     println!("Hello, world!");
