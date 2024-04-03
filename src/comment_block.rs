e45c765cb3e51e84095afde47a341a6e50accf69
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-03 18:38:06 +0200

    chore: improve lisibility in CommentBlock::from_bytes

--- src/comment_block.rs
+++ src/comment_block.rs
@@ -7,9 +7,10 @@ impl BinaryData<CommentBlock> for CommentBlock {
         //TODO: what should we do when bytes.len() == 0 ?
         let are_all_chars_representable = bytes.iter().all(|byte| *byte >= 33 && *byte <= 126);
         
-        match are_all_chars_representable {
-            true => Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())), //safe unwrap because all bytes are valid ASCII
-            false => Err(()) //TODO: explicit error
+        if are_all_chars_representable {
+            Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())) //safe unwrap because all bytes are valid ASCII
+        } else {
+            Err(()) //TODO: explicit error
         }
     }
 
