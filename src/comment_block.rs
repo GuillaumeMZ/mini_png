08707a44430be4a1ffee91c6cb6baaef0a4dc4f5
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-03 22:45:37 +0200

    chore: improve readability

--- src/comment_block.rs
+++ src/comment_block.rs
@@ -9,11 +9,11 @@ impl BinaryData<CommentBlock> for CommentBlock {
         //TODO: what should we do when bytes.len() == 0 ?
         let are_all_chars_representable = bytes.iter().all(|byte| *byte >= 32 && *byte <= 127);
         
-        if are_all_chars_representable {
-            Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())) //safe unwrap because all bytes are valid ASCII
-        } else {
-            Err(anyhow!("Unable to parse a comment: the text is not valid ASCII."))
+        if !are_all_chars_representable {
+            return Err(anyhow!("Unable to parse a comment: the text is not valid ASCII."));
         }
+        
+        Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())) //safe unwrap because all bytes are valid ASCII
     }
 
     fn to_bytes(&self) -> Vec<u8> {
--- src/header_block.rs
+++ src/header_block.rs
@@ -52,6 +52,7 @@ impl fmt::Display for PixelType {
     }
 }
 
+#[derive(Clone, Copy)]
 pub struct HeaderBlock {
     image_width: u32, //must be greater than 0
     image_height: u32, //ditto
@@ -75,30 +76,30 @@ impl HeaderBlock {
 impl BinaryData<HeaderBlock> for HeaderBlock {
     fn from_bytes(bytes: &[u8]) -> Result<HeaderBlock> {
         if bytes.len() != 9 {
-            Err(anyhow!("Unable to parse a header block: not enough bytes to store width + height + pixel type."))
-        } else {
-            //these will never fail because we know that bytes.len() == 9
-            let first_four_bytes: [u8; 4] = bytes[0..=3].try_into().unwrap();
-            let next_four_bytes: [u8; 4] = bytes[4..=7].try_into().unwrap();
-            let last_byte = bytes[8];
-            
-            let image_width = u32::from_be_bytes(first_four_bytes); //TODO: endianess
-            let image_height = u32::from_be_bytes(next_four_bytes);
-
-            if image_width == 0 || image_height == 0 {
-                return Err(anyhow!("Unable to parse a header block: one (or both) of the image's dimension is (are) 0."));
-            }
-
-            if last_byte > 3 { //change according to the supported pixel formats
-                return Err(anyhow!("Unable to parse a header block: {} is not a valid pixel format type.", last_byte));
-            }
-
-            Ok(HeaderBlock {
-                image_width,
-                image_height,
-                pixel_type: last_byte.try_into().unwrap()
-            })
+            return Err(anyhow!("Unable to parse a header block: not enough bytes to store width + height + pixel type."));
         }
+            
+        //these will never fail because we know that bytes.len() == 9
+        let first_four_bytes: [u8; 4] = bytes[0..=3].try_into().unwrap();
+        let next_four_bytes: [u8; 4] = bytes[4..=7].try_into().unwrap();
+        let last_byte = bytes[8];
+        
+        let image_width = u32::from_be_bytes(first_four_bytes);
+        let image_height = u32::from_be_bytes(next_four_bytes);
+
+        if image_width == 0 || image_height == 0 {
+            return Err(anyhow!("Unable to parse a header block: one (or both) of the image's dimension is (are) 0."));
+        }
+
+        if last_byte > 3 { //change according to the supported pixel formats
+            return Err(anyhow!("Unable to parse a header block: {} is not a valid pixel format type.", last_byte));
+        }
+        
+        Ok(HeaderBlock {
+            image_width,
+            image_height,
+            pixel_type: last_byte.try_into().unwrap() //safe unwrap because we checked the value earlier
+        })
     }
 
     fn to_bytes(&self) -> Vec<u8> {
