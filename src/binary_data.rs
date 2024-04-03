21c8e294d37f6cdc74967f30adf4f46d09158afb
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-03 20:48:20 +0200

    Add potential indication in BinaryData

--- src/binary_data.rs
+++ src/binary_data.rs
@@ -2,5 +2,5 @@ use std::vec::Vec;
 
 pub trait BinaryData<T> {
     fn from_bytes(bytes: &[u8]) -> Result<T, ()>;
-    fn to_bytes(&self) -> Result<Vec<u8>, ()>;
+    fn to_bytes(&self) -> Result<Vec<u8>, ()>; //TODO: looks like it never fails. if so, remove result 
 }
