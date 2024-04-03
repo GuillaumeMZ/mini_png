a4ed3cd37c51890aaef1ebb8a14d03c6256566ff
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-03 21:55:57 +0200

    fix: the wrong range was used for representable ascii characters

--- src/comment_block.rs
+++ src/comment_block.rs
@@ -7,7 +7,7 @@ pub struct CommentBlock(String);
 impl BinaryData<CommentBlock> for CommentBlock {
     fn from_bytes(bytes: &[u8]) -> Result<CommentBlock> {
         //TODO: what should we do when bytes.len() == 0 ?
-        let are_all_chars_representable = bytes.iter().all(|byte| *byte >= 33 && *byte <= 126);
+        let are_all_chars_representable = bytes.iter().all(|byte| *byte >= 32 && *byte <= 127);
         
         if are_all_chars_representable {
             Ok(CommentBlock(String::from_utf8(bytes.to_vec()).unwrap())) //safe unwrap because all bytes are valid ASCII
