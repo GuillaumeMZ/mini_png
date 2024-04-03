a8f14491403191bab778dffbb648cd4a689258b5
Guillaume M <58004135+GuillaumeMZ@users.noreply.github.com>
2024-04-03 21:23:28 +0200

    feat: first question programmed (but not working)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,21 @@ mod comment_block;
 mod data_block;
 mod mini_png;
 
+mod commands;
+
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    
+    if args.len() < 2 {
+        eprintln!("Not enough arguments: please provide the path of the file to parse.");
+        std::process::exit(1);
+    }
+
+    let path = Path::new(&args[1]);
+    match commands::question1(path) {
+        Ok(_) => {},
+        Err(error) => { eprintln!("Error while trying to parse the file: {}", error); }
+    }
 }
